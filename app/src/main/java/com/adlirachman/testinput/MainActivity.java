7049a7441f08dd51f83563363362f2cc2cff39ab
Adli Rachman <ragnaboyx@gmail.com>
2018-06-23 14:25:39 +0700

    2

--- app/src/main/java/com/adlirachman/testinput/MainActivity.java
+++ app/src/main/java/com/adlirachman/testinput/MainActivity.java
@@ -10,7 +10,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.landing_page);
+        setContentView(R.layout.activity_loading_screen);
     }
 
     public void toVisPage(View view){
